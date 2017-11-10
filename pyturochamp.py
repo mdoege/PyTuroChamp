a2f324fcecd8f01ed1d45be1a5d664370b18ca9c
Martin Doege <mdoege@compuserve.com>
2017-11-10 17:01:51 +0100

    rest of position values

--- pyturochamp.py
+++ pyturochamp.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-# computer plays as white
+
+# not implemented: castling
 
 import chess as c
 import math
@@ -17,7 +18,7 @@ for x in b.legal_moves:
 	ppv = 0
 	for i in range(64):
 		m = b.piece_at(i)
-		if m and m.piece_type in (c.QUEEN, c.ROOK, c.BISHOP, c.KNIGHT) and m.color == COMPC:
+		if m and m.piece_type in (c.KING, c.QUEEN, c.ROOK, c.BISHOP, c.KNIGHT) and m.color == COMPC:
 			mv_pt, cp_pt = 0, 0
 			a = b.attacks(i)
 			for s in a:
@@ -29,7 +30,7 @@ for x in b.legal_moves:
 				elif e.color == PLAYC:
 					cp_pt += 2
 			ppv += math.sqrt(mv_pt + cp_pt)
-			if m.piece_type != c.QUEEN:
+			if m.piece_type != c.QUEEN and m.piece_type != c.KING:
 				ndef = len(list(b.attackers(COMPC, i)))
 				# defended
 				if ndef == 1:
@@ -37,6 +38,35 @@ for x in b.legal_moves:
 				# twice defended
 				if ndef > 1:
 					ppv += .5
+			# king safety
+			if m.piece_type == c.KING:
+				b2 = c.Board(b.fen())
+				b2.set_piece_at(i, c.Piece(c.QUEEN, COMPC))
+				mv_pt, cp_pt = 0, 0
+				a = b2.attacks(i)
+				for s in a:
+					e = b2.piece_at(s)
+					# empty square
+					if not e:
+						mv_pt += 1
+					# enemy square
+					elif e.color == PLAYC:
+						cp_pt += 2
+				ppv -= math.sqrt(mv_pt + cp_pt)
+		if m and m.piece_type == c.PAWN and m.color == COMPC:
+			# pawn ranks advanced
+			ppv += .2 * (i // 8 - 1)	# assumes computer plays white
+			# pawn defended
+			if len(list(b.attackers(COMPC, i))):
+				ppv += .3
+	# black king
+	if b.is_check():
+		ppv += .5
+	for y in b.legal_moves:
+		b.push(y)
+		if b.is_checkmate():
+			ppv += 1
+		b.pop()
 	print(x, ppv)
 	b.pop()
 
