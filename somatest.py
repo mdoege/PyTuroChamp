ea21cdc80c1a9e39c55f28b4d9d38bb1682b2810
Martin C. Doege <mdoege@compuserve.com>
2018-07-11 04:47:32 +0200

    add SOMA test script and game

---
+++ somatest.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# Show White moves that differ between the recorded SOMA game and soma.py
+
+import chess as c
+import chess.pgn
+import soma as p
+
+pgn = open('soma-mac.pgn')
+game = chess.pgn.read_game(pgn)
+
+b = game.board()
+bad = 0
+o = open('somatest.txt', 'a')
+o2 = open('somatest-diff.txt', 'a')
+
+for m in game.main_line():
+	if b.turn == c.WHITE:
+		t, r = p.getmove(b, usebook = False, silent = True)
+		if r[0] == str(m):
+			o.write('%u %s %s\n' % (b.fullmove_number, m, r[0]))
+		else:
+			o2.write('%u %s %s\n' % (b.fullmove_number, m, r[0]))
+			bad += 1
+	b.push(m)
+
+print('===>', bad, 'moves differ')
+
