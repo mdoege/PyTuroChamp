55b38ed5533008b638e5c973c92736eda2caa220
Martin C. Doege <mdoege@compuserve.com>
2018-07-23 15:38:59 +0200

    Bern: move to own loop

--- bernstein.py
+++ bernstein.py
@@ -336,14 +336,14 @@ def getmove(b, silent = False, usebook = False):
 
 	pmt = get_pmt(b)
 	for n, x in enumerate(b.legal_moves):
-
 		b.push(x)
-		if b.is_checkmate():	# checkmate directly if possible
+		if b.is_checkmate():	# play checkmate directly if possible
 			b.pop()
 			return 1e6, [str(x)]
 		else:
 			b.pop()
 
+	for n, x in enumerate(b.legal_moves):
 		if PMTSTART == 0 and str(x) not in pmt:
 			continue
 		print()
